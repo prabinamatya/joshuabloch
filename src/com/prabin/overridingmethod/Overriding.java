c618797676ee5832723e4c230e929c2f63061a0b
Prabin Amatya <pnamatya@gmail.com>
2015-02-26 13:27:01 -0600

    overriding in java

---
+++ src/com/prabin/overridingmethod/Overriding.java
@@ -0,0 +1,34 @@
+package com.prabin.overridingmethod;
+
+class Wine {
+	String name() {
+		return "wine";
+	}
+}
+
+class SparklingWine extends Wine {
+	@Override
+	String name() {
+		return "Sparkling wine";
+	}
+}
+
+class Champagne extends SparklingWine {
+	@Override
+	String name() {
+		return "champagne";
+	}
+}
+
+public class Overriding {
+	public static void main(String... args) {
+		Wine[] wines = { new Wine(), new SparklingWine(), new Champagne() };
+		
+		for(Wine wine : wines) {
+			System.out.println(wine.name());
+		}
+		
+		Wine wind = new Champagne();
+		System.out.println(wind.name());
+	}
+}
