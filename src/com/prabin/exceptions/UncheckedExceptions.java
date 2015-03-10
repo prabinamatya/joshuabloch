944d7b05133f0dee36ee75c172fe23b99a746d49
Prabin Amatya <pnamatya@gmail.com>
2015-03-10 17:50:45 -0500

    checked exception

--- src/com/prabin/exceptions/UncheckedExceptions.java
+++ src/com/prabin/exceptions/UncheckedExceptions.java
@@ -1,16 +1,26 @@
 package com.prabin.exceptions;
 
 public class UncheckedExceptions {
-	
+
 	/*
-	 * Unchecked exceptions are not checked at compile time. It occurs due to the bad data provided by user during the user-program interaction
+	 * Unchecked exceptions are not checked at compile time. It occurs due to
+	 * the bad data provided by user during the user-program interaction
 	 */
 	public static void main(String... args) {
 		int num1 = 10;
 		int num2 = 0;
-		
-		int res = num1/num2;
-		
+
+		int res = 0;
+		try {
+			res = num1 / num2;
+
+			/*
+			 * Here we are catching checked exception. 
+			 */
+		} catch (ArithmeticException ex) {
+			System.out.println(ex);
+		}
+
 		System.out.println(res);
 	}
 
