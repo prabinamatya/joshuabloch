418dc4a46da4c32edec1162075d6dfff24e0ca5d
Prabin Amatya <pnamatya@gmail.com>
2015-03-15 09:45:56 -0500

    multiple locks

---
+++ src/com/prabin/multithreading/MultipleLocks.java
@@ -0,0 +1,8 @@
+package com.prabin.multithreading;
+
+public class MultipleLocks {
+	public static void main(String... args) {
+		WorkerMultiple worker = new WorkerMultiple();
+		worker.main();
+	}
+}
