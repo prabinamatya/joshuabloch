2e7e7566e5bddbdac34b88646f4ee732a746254f
Prabin Amatya <pnamatya@gmail.com>
2015-03-19 16:39:57 -0500

    wait notify

---
+++ src/com/prabin/multithreading/WaitNotify.java
@@ -0,0 +1,65 @@
+package com.prabin.multithreading;
+
+import java.util.Scanner;
+
+public class WaitNotify {
+	public static void main(String[] args) throws InterruptedException {
+		final ProcessWaitNotify processor = new ProcessWaitNotify();
+		
+		Thread t1 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					processor.produce();
+				} catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		Thread t2 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					processor.consume();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		
+		t1.join();
+		t2.join();
+	}
+}
+
+class ProcessWaitNotify  {
+
+	public void produce() throws InterruptedException {
+		synchronized(this) {
+			System.out.println("Producer thread running....");
+			wait();
+			System.out.println("Resumed...");
+		}
+	}
+
+	public void consume() throws InterruptedException {
+		Scanner scanner = new Scanner(System.in);
+		Thread.sleep(2000);
+		
+		synchronized (this) {
+			System.out.println("Waiting for return key.");
+			scanner.nextLine();
+			System.out.println("Return key pressed");
+			notify();
+			Thread.sleep(5000);
+		}
+		
+	}
+	
+}
