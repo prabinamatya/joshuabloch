9e67ef88356357cbedc69f591a69d8ad3733053e
Prabin Amatya <pnamatya@gmail.com>
2015-03-26 17:15:18 -0500

    semaphores

--- src/com/prabin/multithreading/SemaphoreApp.java
+++ src/com/prabin/multithreading/SemaphoreApp.java
@@ -4,6 +4,10 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+
+//Semaphores are mainly used to limit the number of simultaneous threads that can access a
+//resource, but you can also use them to implement deadlock recovery systems. Semaphores with one permit is basically
+//a lock that you can unlock from other threads
 public class SemaphoreApp {
 	
 	public static void main(String[] args) throws InterruptedException {
