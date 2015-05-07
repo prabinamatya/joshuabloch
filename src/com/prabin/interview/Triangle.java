191cb0652c46acf7a2152f7340cd4cbaf7eb2ea6
Prabin Amatya <pnamatya@gmail.com>
2015-05-06 21:45:21 -0500

    integer return bigger

--- src/com/prabin/interview/Triangle.java
+++ src/com/prabin/interview/Triangle.java
@@ -1,28 +1,58 @@
 package com.prabin.interview;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Triangle {
 
 	public void printTriangle(char[] val) {
-		for(int i = 1; i < val.length; i++) {
-			for(int j = 0; j < i; j++) {
+		for (int i = 1; i < val.length; i++) {
+			for (int j = 0; j < i; j++) {
 				System.out.print(val[j]);
 			}
 			System.out.println();
 		}
 	}
-	
+
 	public String[] combine(int[] a, String[] b) {
 		int lengthOfFinalArray = a.length + b.length;
 		String[] combine = new String[lengthOfFinalArray];
-		
-		for(int i=0, j=0; j<lengthOfFinalArray && i < a.length; i++, j++) {
+
+		for (int i = 0, j = 0; j < lengthOfFinalArray && i < a.length; i++, j++) {
 			combine[j] = String.valueOf(a[i]);
 			combine[++j] = b[i];
-			
+
 		}
 		return combine;
+	}
+
+	public int biggestNumber(int... a) {
+		 int[] sortedArray = sortedBySize(a);
+		 StringBuilder sb = new StringBuilder();
+		 for(int val : sortedArray) {
+			 sb.append(val);
+		 }
+		 return Integer.valueOf(sb.toString());
+	}
+
+	public int[] sortedBySize(int... a) {
+		int temp;
+		for (int i = 0; i < a.length - 1; i++) {
+			for (int j = 1; j < a.length-i; j++) {
+
+				if (firstDigit(a[j]) > firstDigit(a[j - 1])) {
+					temp = a[j-1];
+					a[j-1] = a[j];
+					a[j] = temp;
+				}
+			}
+		}
+		System.out.println(a);
+		return a;
+	}
+
+	private int firstDigit(int num) {
+		if (num < 10) {
+			return num;
+		}
+		int i = num / 10;
+		return i;
 	}
 }
--- test/com/prabin/interview/TriangleTest.java
+++ test/com/prabin/interview/TriangleTest.java
@@ -1,5 +1,7 @@
 package com.prabin.interview;
 
+import static org.junit.Assert.*;
+
 import java.util.Arrays;
 
 import org.junit.Ignore;
@@ -17,11 +19,27 @@ public class TriangleTest {
 	}
 
 	@Test
+	@Ignore
 	public void combineValues() throws Exception {
 		int a[] = {1, 2, 3};
 		String b[] = {"a", "b", "c"};
-		String[] actual = testObject.combine(a, b);
 		String[] expected = {"1", "a", "2", "b", "3", "c"};
-		Arrays.equals(expected, actual);
+		assertArrayEquals(expected, testObject.combine(a, b));
+	}
+	
+	@Test
+	public void combineToBiggest() throws Exception {
+		int [] a = {5, 20, 9, 1};
+		int actual = testObject.biggestNumber(a);
+		assertEquals(95201, actual);
+	}
+	
+	@Test
+//	@Ignore
+	public void sortBiggestNumber() throws Exception {
+		int [] a = {5, 20, 9, 1};
+		int[] actual = testObject.sortedBySize(a);
+		int [] expected = {9, 5, 20, 1};
+		assertArrayEquals(expected, actual);
 	}
 }
